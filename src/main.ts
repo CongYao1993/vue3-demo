d26063defd5b77797097eeaef40ff06c9c041099
yaocong01 <cong1207@qq.com>
2024-06-13 23:02:22 +0800

    修改路由

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 import { createApp } from "vue";
-import { createRouter,createWebHashHistory  } from "vue-router";
-import "@/assets/base.css";
 import App from "@/App.vue";
-import Home from "@/pages/home.vue";
-import Blog from "@/pages/blog.vue";
+import { createMyRouter } from '@/router/index'
+import "@/assets/base.css";
 
-const routes = [
-    { path: "/", component: Home },
-    { path: "/blog", component: Blog },
-];
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-});
 
 const app = createApp(App);
-app.use(router);
+app.use(createMyRouter());
 app.mount("#app");
